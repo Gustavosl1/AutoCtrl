709b18730f4c4e185a1d1cb13309cdc33dec3b90
Gustavo Lima <gusouzalima5@gmail.com>
2025-04-08 20:27:51 -0300

    commit-version 1.0

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+
+const form = document.getElementById('formulario');
+const resultado = document.getElementById('resultado');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const respostas = form.querySelectorAll('select');
+  let pontuacao = 0;
+
+  respostas.forEach((select) => {
+    pontuacao += parseInt(select.value);
+  });
+
+  let mensagem = "";
+
+  if (pontuacao <= 6) {
+    mensagem = "✅ Seu foco está equilibrado. Continue mantendo hábitos saudáveis!";
+  } else if (pontuacao <= 12) {
+    mensagem = "⚠️ Atenção! Você apresenta alguns sinais de dificuldade de foco.";
+  } else {
+    mensagem = "🚨 Seu nível de distração é alto. Recomendamos procurar orientação profissional.";
+  }
+
+  resultado.innerText = mensagem;
+  resultado.style.display = "block";
+});
